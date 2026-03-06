77456d0c6c67d6e73b7efac9fcf862fb5b76eb87
Quentin Geerts <geerts.quentin@gmail.com>
2026-03-06 16:34:11 +0100

    Correction de l'exercice sur les boucles et le DOM

--- jour2/correction-exercice-tables/script.js
+++ jour2/correction-exercice-tables/script.js
@@ -7,3 +7,41 @@
   Bonus:
   - Permettre à l'utilisateur de choisir l'opération (+, -, *, /)
 */
+
+window.addEventListener("DOMContentLoaded", () => {
+  const select = document.getElementById("table");
+  const btnCalcul = document.querySelector("#btnCalcul");
+  const divResult = document.querySelector("div#result");
+
+  let firstLoad = true;
+
+  window.addEventListener("keypress", (e) => {
+    if (select.value != "" && e.key === "Enter") calcul();
+  });
+
+  select.addEventListener("click", () => {
+    if (firstLoad) {
+      select.removeChild(select.children[0]);
+      initSelect();
+      firstLoad = false;
+    }
+  });
+  
+  btnCalcul.addEventListener("click", calcul);
+
+  function initSelect() {
+    for (let i = 1; i <= 20; i++) {
+      const option = select.appendChild(document.createElement("option"));
+      option.value = i;
+      option.textContent = "Table de " + i;
+    }
+  }
+
+  function calcul() {
+    divResult.textContent = "";
+    for (let i = 1; i <= 20; i++) {
+      const p = divResult.appendChild(document.createElement("p"));
+      p.textContent = `${i} * ${select.value} = ${i * select.value}`;
+    }
+  }
+});

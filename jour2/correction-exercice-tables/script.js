8f1d4ca690a9b63f2770524dd07727fec60eaafb
Quentin Geerts <geerts.quentin@gmail.com>
2026-03-06 14:20:15 +0100

    Énoncé de l'exercice sur les boucles avec le DOM

---
+++ jour2/correction-exercice-tables/script.js
@@ -0,0 +1,6 @@
+/*
+  Exercice : Table de multiplication
+  Réaliser un programme qui permet de demander à l'utilisateur la sélection depuis une liste déroulante, la table de multiplication à afficher (calculs de 1 à 20).
+
+  La liste déroulante doit être générée automatiquement à l'ouverture de la page.
+*/

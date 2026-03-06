48e475ba4c1fa06a544a53bda2bcbe816589cd35
Quentin Geerts <geerts.quentin@gmail.com>
2026-03-06 14:39:17 +0100

    Ajout d'un bonus

--- jour2/correction-exercice-tables/script.js
+++ jour2/correction-exercice-tables/script.js
@@ -3,4 +3,7 @@
   Réaliser un programme qui permet de demander à l'utilisateur la sélection depuis une liste déroulante, la table de multiplication à afficher (calculs de 1 à 20).
 
   La liste déroulante doit être générée automatiquement à l'ouverture de la page.
+
+  Bonus:
+  - Permettre à l'utilisateur de choisir l'opération (+, -, *, /)
 */
